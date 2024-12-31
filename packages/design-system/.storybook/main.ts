64544089e8befd9054f254336eee5bf29169d9a4
DuetoPark <inhye.park.dev@gmail.com>
2025-01-01 02:45:32 +0900

    feat(design-system): storybook 설정

---
+++ packages/design-system/.storybook/main.ts
@@ -0,0 +1,38 @@
+import { join, dirname } from "path";
+import { mergeConfig } from "vite";
+import type { StorybookConfig } from "@storybook/react-vite";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+  core: {
+    builder: "@storybook/builder-vite", // 👈 The builder enabled here.
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ["storybook-dark-mode"],
+      },
+    });
+  },
+};
+export default config;
