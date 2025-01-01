8f5ebc38c16ad1b1d3dcfa5c9a40b4451a350a3b
inhye94 <inhye.park.dev@gmail.com>
2025-01-01 22:42:32 +0900

    chore(project-1): .gitignore 업데이트

--- apps/next-project-1/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/bag-inhye/rich_inhye/project/portfolio-2025/example/apps/next-project-1/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
