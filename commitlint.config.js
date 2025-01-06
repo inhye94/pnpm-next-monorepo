733d008f782c7212ea54c60077eaae1465214111
inhye94 <inhye.park.dev@gmail.com>
2025-01-06 17:45:15 +0900

    chore(root): commitlint type-enum 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "init",
+        "feat",
+        "chore",
+        "style",
+        "fix",
+        "test",
+        "deploy",
+        "refactor",
+        "rename",
+        "remove",
+        "docs",
+      ],
+    ],
+  },
 };
