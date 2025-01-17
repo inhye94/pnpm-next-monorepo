b29113c7febb4e5ef96279cb5128df73cc8050ef
inhye94 <inhye.park.dev@gmail.com>
2025-01-17 15:21:06 +0900

    feat(portfolio): mock 데이터 추가

--- apps/portfolio/app/components/Career.tsx
+++ apps/portfolio/app/components/Career.tsx
@@ -1,18 +1,9 @@
-interface ICareerItem {
+export interface ICareerItem {
   id: string;
   corp: string;
   summary: string;
   roles: string[];
 }
-
-export const careers: ICareerItem[] = [
-  {
-    id: "career-id-1",
-    corp: "회사명",
-    summary: "서비스 요약",
-    roles: ["역할1", "역할2", "역할3"],
-  },
-];
 
 /**
  * <카드>
--- apps/portfolio/app/components/Project.tsx
+++ apps/portfolio/app/components/Project.tsx
@@ -5,11 +5,6 @@ interface IDetailImages {
   content?: string[];
 }
 
-interface IContents {
-  title: string;
-  contents: string[];
-}
-
 interface ITroubleShooting {
   title: string;
   problem: string;
@@ -18,11 +13,12 @@ interface ITroubleShooting {
 }
 
 interface ILink {
+  id: string;
   title: string;
   link: string;
 }
 
-interface IProjectItem {
+export interface IProjectItem {
   id: string;
   title: string;
   summary: string;
@@ -32,16 +28,15 @@ interface IProjectItem {
   screens: IDetailImages[];
   stacks: string[];
   background: string;
-  highlights: string[];
+  highlights?: string[];
   feature?: string[];
-  troubleShooting: ITroubleShooting[];
-  learnings: IContents[];
+  troubleShooting?: ITroubleShooting[];
+  learnings: string[];
   links: {
     blog?: ILink[];
     github: string;
     deploy?: string;
   };
-  setup: string;
 }
 
 /**
@@ -72,39 +67,6 @@ interface IProjectItem {
 ---
  * 링크
  */
-
-export const projects: IProjectItem[] = [
-  {
-    id: "project-id-1",
-    title: "프로젝트명",
-    summary: "프로잭트 개요",
-    period: "언제부터 ~ 언제까지",
-    team: "개인 프로젝트 / 팀 프로젝트 (몇 명)",
-    thumbnailUrl: "이미지 주소",
-    screens: [
-      {
-        id: "screen-id-1",
-        url: "이미지 주소",
-        title: "페이지명",
-        content: ["기능1", "기능2", "기능3"],
-      },
-    ],
-    stacks: ["스택1", "스택2", "스택3"],
-    background: "개발 배경을 이러쿵 저러쿵",
-    highlights: ["작업1", "작업2", "작업3"],
-    feature: ["기능1", "기능2", "기능3"],
-    troubleShooting: [
-      { title: "주제", problem: "문제점", solution: "해결법", outcome: "성과" },
-    ],
-    learnings: [{ title: "주제", contents: ["배운점1", "배운점2", "배운점3"] }],
-    links: {
-      blog: [{ title: "블로그 주제", link: "블로그 링크" }],
-      github: "깃허브 링크",
-      deploy: "배포 링크",
-    },
-    setup: "셋업 방법",
-  },
-];
 
 const Project = () => {
   return (
