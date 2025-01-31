f3b7a7e901f41d68db46d5226fbd903aa5150529
inhye94 <inhye.park.dev@gmail.com>
2025-02-01 00:18:42 +0900

    fix(design-system): alias 에러 대응

--- packages/design-system/src/components/index.tsx
+++ packages/design-system/src/components/index.tsx
@@ -1,8 +1,9 @@
 export { default as BaseBadge } from "./badge/BaseBadge";
 export { default as BaseButton } from "./button/BaseButton";
 export { default as CheckboxGroup } from "./checkboxGroup/CheckboxGroup";
-export * from "./icon";
+export { default as Icon } from "./icon";
 export { default as BaseInput } from "./input/BaseInput";
 export { default as Container } from "./layout/Container";
+export { default as Modal } from "./modal/BaseModal";
 export { default as RadioGroup } from "./radioGroup/RadioGroup";
 export { default as TextField } from "./textField/TextField";
--- packages/design-system/src/main.tsx
+++ packages/design-system/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import "@styles/main.css";
 import App from "./App";
+import "./styles/main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
