47ed7be65aefaa365048ae9fba83c5fcbe533842
inhye94 <inhye.park.dev@gmail.com>
2025-02-21 17:48:50 +0900

    feat(portfolio-admin): Sidebar 컴포 생성

--- apps/portfolio-admin/src/components/Sidebar.tsx
+++ apps/portfolio-admin/src/components/Sidebar.tsx
@@ -1,16 +1,49 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { useCallback, useState } from "react";
 
-const menus: string[] = ["career", "project", "skill"];
+// data
+const menus: { id: string; title: string; path: string }[] = [
+  { id: "menu1", title: "career", path: "career" },
+  { id: "menu2", title: "project", path: "project" },
+  { id: "menu3", title: "skill", path: "skill" },
+];
 
-export default function Sidebar() {
+// type
+interface SidebarProps {
+  id: string;
+}
+
+// component
+export default function Sidebar({ id }: SidebarProps) {
+  const [currentMenu, setCurrentMenu] = useState<string | null>(null);
+
+  const handleMenuClick = useCallback((id: string) => {
+    setCurrentMenu(id);
+  }, []);
+
   return (
     <nav>
-      <h2>메뉴</h2>
+      <h2 className="visually-hidden">메뉴</h2>
 
       <ul>
-        {menus.map((menu, index) => (
-          <li key={`${menu}-detail-${index}`}>
-            <Link href={`/123/${menu}`}>{menu} detail</Link>
+        {menus.map((menu) => (
+          <li
+            key={menu.id}
+            className="bg-background-gray hover:bg-background-darkgray group my-4 overflow-hidden rounded-full transition-colors"
+          >
+            <Link
+              href={`/${id}/${menu.path}`}
+              className={classNames(
+                "text-16 hover:text-neutral-10 inline-block w-full px-16 py-8 font-semibold capitalize transition-all group-hover:translate-x-4",
+                currentMenu === menu.id ? "text-neutral-10" : "text-neutral-50",
+              )}
+              onClick={() => handleMenuClick(menu.id)}
+            >
+              {menu.title} detail
+            </Link>
           </li>
         ))}
       </ul>
