dfb13ed842f1474cdb92f9f6dfd9f06da1bc06e4
inhye94 <inhye.park.dev@gmail.com>
2025-02-24 20:57:07 +0900

    feat(portfolio-admin): Loading 컴포 생성

---
+++ apps/portfolio-admin/app/loading.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Loading",
+};
+
+export default function Loading() {
+  return (
+    <div>
+      <h2>Loading...</h2>
+    </div>
+  );
+}
