e025e0ed0f4b7ea37689d8ca595db84110c91c28
inhye94 <inhye.park.dev@gmail.com>
2025-03-13 22:26:21 +0900

    init(admin): vite react 프로젝트 생성

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,11 @@
+import { BaseButton } from "@workspace/design-system/components";
+
+function App() {
+  return (
+    <>
+      <BaseButton>asd</BaseButton>
+    </>
+  );
+}
+
+export default App;
