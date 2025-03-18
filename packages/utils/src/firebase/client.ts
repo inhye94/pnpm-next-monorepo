55bc715fdc23f5f79d2212d4eeeb888b2f4ac9af
inhye94 <inhye.park.dev@gmail.com>
2025-03-19 01:01:41 +0900

    fix(utils): firebase config에 환경변수 분기 처리 (서버/클라이언트)

--- packages/utils/src/firebase/client.ts
+++ packages/utils/src/firebase/client.ts
@@ -2,22 +2,49 @@ import dotenv from "dotenv";
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { child, get, getDatabase, ref } from "firebase/database";
 
-// NODE_ENV에 따라 올바른 환경 변수 파일 로드
-const NODE_ENV = process.env.NODE_ENV || "development";
-dotenv.config({
-  path: NODE_ENV === "production" ? ".env.production" : "../../.env",
-});
+// 서버 환경 체크
+const isServer = typeof window === "undefined";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+if (isServer) {
+  // NODE_ENV에 따라 올바른 환경 변수 파일 로드
+  const NODE_ENV = process.env.NODE_ENV || "development";
+  dotenv.config({
+    path: NODE_ENV === "production" ? "../../.env.production" : "../../.env",
+  });
+}
+
+let firebaseConfig = null;
+
+if (isServer) {
+  firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+} else {
+  firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
+}
+
+/**
+ * 만약, Vite 프로젝트에서 .env는 프로젝트 루트에서 .env 파일을 자동 로드한다.
+ * Vite 프로젝트에 VITE_ 프리픽스를 붙인 .env 파일을 만든다.
+ *
+ * 테스트 마치면 console은 주석 처리하기
+ */
 
 // NOTE: !getApps().length 확인은 어플리케이션을 리로드 할 때 실수로 SDK를 다시 초기화 하는 것을 방지
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
