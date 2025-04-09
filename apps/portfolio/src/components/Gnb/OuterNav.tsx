85a0210a192afb34108dd052fbb19a96d633c581
inhye94 <inhye.park.dev@gmail.com>
2025-04-09 09:14:37 +0900

    style(portfolio): OuterNav에 Icon 추가

--- apps/portfolio/src/components/Gnb/OuterNav.tsx
+++ apps/portfolio/src/components/Gnb/OuterNav.tsx
@@ -1,8 +1,12 @@
+import { Icon } from "@workspace/design-system/components";
+import { iconMap } from "@workspace/utils/shared";
+
 // type
 interface IOuterNavMenu {
   id: string;
   text: string;
   url: string;
+  icon: keyof typeof iconMap;
 }
 
 // data
@@ -11,11 +15,13 @@ const outerNav: IOuterNavMenu[] = [
     id: "outerNav-1",
     text: "깃허브",
     url: "https://github.com/inhye94",
+    icon: "github",
   },
   {
     id: "outerNav-2",
     text: "블로그",
     url: "https://velog.io/@inhye94/posts",
+    icon: "velog",
   },
 ];
 
@@ -32,10 +38,11 @@ export default function OuterNav() {
             className="relative after:absolute after:bottom-0 after:left-1/2 after:block after:h-1 after:w-0 after:-translate-x-1/2 after:bg-white after:transition-all after:content-[''] hover:after:w-full"
           >
             <a
-              className="text-neutral-99 text-14 inline-block py-4 font-light whitespace-nowrap"
+              className="text-neutral-99 text-14 flex items-center gap-4 py-4 font-light whitespace-nowrap"
               href={menu.url}
               target="_blank"
             >
+              <Icon className="hidden md:inline" name={menu.icon} />
               {menu.text}
             </a>
           </li>
